6e48b3b9bd78a063c7a23365d3ed96ef7eb735a6
MASisserson <masisserson@gmail.com>
2020-10-26 14:16:28 -0400

    Finished lesson 2 assignments first draft. Pending code reviews.

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,38 +1,56 @@
 # Calculator.rb
-
 # ask the user for two numbers
 # ask the user for an operation to perform
 # perform the operation on the two numbers
 # output the result
 
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
 def prompt(message)
   puts "=> #{message}"
 end
 
-def valid_number?(num)
-  /^-?\d+$/.match(num)
+def valid_number?(input)
+  /\d/.match(input) && /^-?\d*\.?\d*$/.match(input)
 end
 
 def operation_to_message(op)
-  case op
-  when '1'
-    'Adding'
-  when '2'
-    'Subtracting'
-  when '3'
-    'Multiplying'
-  when '4'
-    'Dividing'
+  message = case op
+            when '1'
+              'Adding'
+            when '2'
+              'Subtracting'
+            when '3'
+              'Multiplying'
+            when '4'
+              'Dividing'
+            end
+
+  message
+end
+
+def another_calculation?
+  loop do
+    prompt MESSAGES['another']
+    answer = gets.chomp
+    if /^y$/i.match(answer) || /^[y][e][s]$/i.match(answer)
+      return true
+    elsif /^n$/i.match(answer) || /^[n][o]$/i.match(answer)
+      return false
+    else
+      prompt MESSAGES['not_valid_answer']
+    end
   end
 end
 
-prompt "Welcome to Calculator! Enter your name:"
+prompt MESSAGES['welcome']
 
 name = String.new
 loop do
   name = gets.chomp
   if name.empty?
-    prompt "Make sure you use a valid name."
+    prompt MESSAGES['valid_name']
   else
     break
   end
@@ -43,25 +61,25 @@ prompt "Hi #{name}!"
 loop do # main loop
   number1 = String.new
   loop do
-    prompt "What's the first number?"
+    prompt MESSAGES['first_number']
     number1 = gets.chomp
 
     if valid_number?(number1)
       break
     else
-      prompt "Hm... that doesn't look liek a valid number."
+      prompt MESSAGES['not_valid_number']
     end
   end
 
   number2 = String.new
   loop do
-    prompt "What's the second number?"
+    prompt MESSAGES['second_number']
     number2 = gets.chomp
 
     if valid_number?(number2)
       break
     else
-      prompt "Hm... that doesn't look liek a valid number."
+      prompt MESSAGES['not_valid_number']
     end
   end
 
@@ -81,7 +99,7 @@ What operation would you like to perform?
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt "Must choose 1, 2, 3, or 4"
+      prompt MESSAGES['not_valid_operation']
     end
   end
 
@@ -89,20 +107,18 @@ What operation would you like to perform?
 
   result = case operator
            when '1'
-             number1.to_i + number2.to_i
+             number1.to_f + number2.to_f
            when '2'
-             number1.to_i - number2.to_i
+             number1.to_f - number2.to_f
            when '3'
-             number1.to_i / number2.to_i
+             number1.to_f * number2.to_f
            when '4'
              number1.to_f / number2.to_f
            end
 
   prompt "The result is #{result}"
 
-  prompt "Do you want to perform another calculation? (Y to calculate again)"
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  break unless another_calculation?
 end
 
-prompt "Thank you for using the calculator. Good bye!"
+prompt MESSAGES['goodbye']
--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,57 +1,147 @@
 # rock_paper_scissors.rb
 
-VALID_CHOICES = %w(rock paper scissors)
+# Keeping Track of wins and losses
+# 2 options when you enter the game:
+# Best of one
+# Best of 9 (5 wins)
+# At end, computer asks if you are done or if you want to play a best of 9.
+# If say yes, you start playing a best of 9.
+
+VALID_CHOICES = %w(rock paper scissors spock lizard)
+ALLOWED_INPUT = %w(r ro roc rock
+                   p pa pap pape paper
+                   s sc sci scis sciss scisso scissor scissors
+                   sp spo spoc spock
+                   l li liz liza lizar lizard)
 
 def prompt(message)
   puts "=> #{message}"
 end
 
-def convert(input)
-  case input
-  when 'rock'     then 1
-  when 'paper'    then 2
-  when 'scissors' then 3
+def true_feelings(message)
+  puts "$$ #{message}"
+end
+
+def calculate_result(player, computer)
+  score = VALID_CHOICES.index(player) - VALID_CHOICES.index(computer)
+  if [1, 3, -2, -4].include?(score)
+    1
+  elsif [2, 4, -1, -3].include?(score)
+    -1
+  else
+    0
   end
 end
 
-def display_result(player, computer)
-  result = (convert(player) - convert(computer))
-
-  case result
+def display_result(score)
+  case score
   when 1  then prompt "You won!"
-  when 2  then prompt "Computer won!"
   when -1 then prompt "Computer won!"
-  when -2 then prompt "You won!"
   when 0  then prompt "It's a tie!"
   end
 end
 
+def display_score(player, computer)
+  prompt "Player: #{player}"
+  prompt "Computer: #{computer}"
+end
+
+def convert_to_valid_choice!(input)
+  allowed_index = ALLOWED_INPUT.index(input)
+
+  case allowed_index
+  when 0..3   then input.clear.concat('rock')
+  when 4..8   then input.clear.concat('paper')
+  when 10..16 then input.clear.concat('scissors')
+  when 17..20 then input.clear.concat('spock')
+  when 21..26 then input.clear.concat('lizard')
+  end
+end
+
+player_score = 0
+computer_score = 0
+best_of_nine = false
+first_round = true
+answer = ''
 loop do
   choice = String.new
   loop do
     prompt "Choose one: #{VALID_CHOICES.join(', ')}"
-    choice = gets.chomp
+    choice = gets.chomp.downcase.strip
 
-    if VALID_CHOICES.include?(choice)
+    if ALLOWED_INPUT.include?(choice)
       break
     else
       prompt "That's not a valid choice."
     end
   end
 
+  if choice == 's'
+    loop do
+      confirmation_prompt = <<-MSG
+What did you mean by 's'?
+    1. scissors
+    2. spock
+  MSG
+      prompt confirmation_prompt
+      decision = gets.chomp.downcase.strip
+
+      if %w(scissors 1).include?(decision)
+        input.clear.concat('scissors')
+        break
+      elsif %w(spock 2).include?(decision)
+        input.clear.concat('spock')
+        break
+      else
+        prompt "I'm sorry. I'm not made to accept that answer."
+      end
+    end
+  end
+
+  convert_to_valid_choice!(choice)
+
   computer_choice = VALID_CHOICES.sample
 
-  puts "You chose: #{choice}; Computer chose: #{computer_choice}"
+  prompt "You chose: #{choice}; Computer chose: #{computer_choice}"
 
-  display_result(choice, computer_choice)
-  # captures a piece of logic in one line
+  result = calculate_result(choice, computer_choice)
+  display_result(result)
 
-  prompt "Do you want to play again?"
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  case result
+  when 1  then player_score += 1
+  when -1 then computer_score += 1
+  end
+  display_score(player_score, computer_score)
+
+  if first_round
+    first_round = false
+    true_feelings "Is that all you've got? Let's make this a best of 9."
+    true_feelings "Wad'ya say?"
+    prompt "Type 'yes' if you wish to play again."
+    answer = gets.chomp.downcase.strip
+  end
+  if answer.start_with?('y')
+    best_of_nine = true
+  end
+  break unless best_of_nine
+  break if player_score > 4 || computer_score > 4
 end
 
-prompt "Thank you for playing. Good bye!"
+true_feelings "I was just starting ot have fun..." if !best_of_nine
+final_score_message = <<-MSG
+Final Score:
+Player: #{player_score}
+Computer: #{computer_score}
+MSG
+
+if player_score >= 5
+  true_feelings "Well done. You got the best of me."
+  true_feelings "I'm gonna go sulk now."
+  prompt final_score_message
+elsif computer_score >= 5
+  true_feelings "Well would you look at that."
+  prompt final_score_message
+end
 
 # Final Thoughts:
 # Ruby seems to search above a command for the requisite method.
@@ -61,7 +151,9 @@ prompt "Thank you for playing. Good bye!"
 # before calling the display_results method
 
 # I feel like the creation of another method and the use of case statements
-# as above is a bit brutal, as far as answers go, but it does get rif of
+# as above is a bit blunt, as far as answers go, but it does get rid of
 # the rubocop complaint about there being too much complexity to the if
 # statements that existed before. I also don't like that the logic of the
 # rock paper scissors game is not entirely clear from the case statements.
+
+# Keep in mind this trick for later: using CONSTANTS for repeated arrays
